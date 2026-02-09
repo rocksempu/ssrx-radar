4d407a20b0e5d54790cf97c6664ae50ba535525c
Fabio Paixao <fpaixao.albuquerque@gmail.com>
2026-02-08 22:20:00 -0300

    change push-client.js

--- push-client.js
+++ push-client.js
@@ -4,18 +4,28 @@ async function enablePush() {
     return;
   }
 
+  // 🔐 ID persistente do device (LGPD safe)
+  const deviceId =
+    localStorage.getItem("ssrx_device_id") ||
+    crypto.randomUUID();
+
+  localStorage.setItem("ssrx_device_id", deviceId);
+
   const reg = await navigator.serviceWorker.ready;
 
   const sub = await reg.pushManager.subscribe({
     userVisibleOnly: true,
-    applicationServerKey: "BE_VCtBlAYJe2avtB_5_PFxROofwUwLtJ5y7HUOoyuUWkSxrb-_dmjtdkx3iSbKvdTviruWRfUyYLPZ_C4YKFwY"
+    applicationServerKey:
+      "BE_VCtBlAYJe2avtB_5_PFxROofwUwLtJ5y7HUOoyuUWkSxrb-_dmjtdkx3iSbKvdTviruWRfUyYLPZ_C4YKFwY"
   });
 
   await fetch("/.netlify/functions/push", {
     method: "POST",
+    headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
       type: "subscribe",
-      sub: sub
+      sub: sub,
+      deviceId: deviceId
     })
   });
 
